b119f7abbe367044a04243b1f6e949f6c4fd390f
vidankov <slavavinsent@gmail.com>
2025-02-17 12:06:08 +0300

    Описаны переменные и ввод данных для решения задачи

---
+++ 007-demo-tip-calculator/solution.js
@@ -0,0 +1,3 @@
+let clientsNumber = parseInt(prompt("—колько посетителей включить в счЄт?"));
+let bill = parseInt(prompt("¬ведите сумму счЄта"));
+let tips = parseInt(prompt("—колько процентов чаевых хотите оставить?"));
