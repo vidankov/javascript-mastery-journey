9c89f40dd037e4372c90a2e4ca4a4e6789987ad4
vidankov <slavavinsent@gmail.com>
2025-02-17 12:07:28 +0300

    Исправлено преобразование вводимых данных: раньше к int, теперь к float

--- 007-demo-tip-calculator/solution.js
+++ 007-demo-tip-calculator/solution.js
@@ -1,3 +1,7 @@
 let clientsNumber = parseInt(prompt("—колько посетителей включить в счЄт?"));
-let bill = parseInt(prompt("¬ведите сумму счЄта"));
-let tips = parseInt(prompt("—колько процентов чаевых хотите оставить?"));
+let bill = parseFloat(prompt("¬ведите сумму счЄта"));
+let tips = parseFloat(prompt("—колько процентов чаевых хотите оставить?"));
+
+let billWithTips = bill * (1 + tips / 100);
+
+alert();
