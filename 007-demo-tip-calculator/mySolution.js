0200eedb3d41dae22e17ee6b686a3a8bf1678347
vidankov <slavavinsent@gmail.com>
2025-02-17 13:27:06 +0300

    Выполнено практическое задание

---
+++ 007-demo-tip-calculator/mySolution.js
@@ -0,0 +1,8 @@
+﻿let clientsNumber = parseInt(prompt("Сколько посетителей включить в счёт?"));
+let bill = parseFloat(prompt("Введите сумму счёта"));
+let tips = parseFloat(prompt("Сколько процентов чаевых хотите оставить?"));
+
+let billWithTips = bill * (1 + tips / 100);
+let costPerPerson = billWithTips / clientsNumber;
+
+alert(`Общий счёт: ${billWithTips}\nС человека: ${costPerPerson}`);
--- 007-demo-tip-calculator/solution.js
+++
@@ -1,7 +0,0 @@
-let clientsNumber = parseInt(prompt("—колько посетителей включить в счЄт?"));
-let bill = parseFloat(prompt("¬ведите сумму счЄта"));
-let tips = parseFloat(prompt("—колько процентов чаевых хотите оставить?"));
-
-let billWithTips = bill * (1 + tips / 100);
-
-alert();
